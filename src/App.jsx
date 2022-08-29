e0357e23690d4ef3b540cd19d46d6d2a742d35c8
Jesus Lencinas <jesu9556@gmail.com>
2022-08-29 13:01:51 -0300

    creandoContador

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-
+import contador from './components/contador';
 
 function Application() {
   return (
       <>
         <NavBar/>
         <ItemListContainer contenido="1"/>
-        
+        <contador/>
       </>
   );
 }
---
+++ src/components/contador.jsx
@@ -0,0 +1,14 @@
+const contador = () => {
+
+    return (
+        <div className='counter'>
+            <button>-</button>
+            <span>2</span>
+            <button>+</button>
+        <div>
+            <button>Agregar al carrito</button>
+        </div>
+        </div>
+    );
+}
+export default contador;
